c37ed4e45fa40abcbb69054bc0f2d6805820a382
Andy Strube <andystrube64@gmail.com>
2020-07-16 14:41:05 -0700

    Added a search bar to the homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,12 @@ class StaticPagesController < ApplicationController
   def index
     @channels = Channel.all
     @posts = Post.order("created_at DESC").page(params[:page]).per_page(2)
+    @search = params["search"]
+    if @search.present?
+      @title = @search["title"]
+      @posts = Post.where("title ILIKE ?", "%#{@title}%")
+      @posts = @posts.order("created_at DESC").page(params[:page]).per_page(2)
+    end
   end
 
 end
